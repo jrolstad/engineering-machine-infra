3809ef15b5a47c08e2ce4d51e4ecc49adfcb0461
Joshua Rolstad <joshuarolstad@precocityllc.com>
2023-06-12 21:05:42 -0700

    Removing lock to make things easier

--- tf/resource_groups.tf
+++ tf/resource_groups.tf
@@ -7,10 +7,3 @@ resource "azurerm_resource_group" "primary" {
     service     = var.service_name
   }
 }
-
-resource "azurerm_management_lock" "resource_group_lock" {
-  name       = azurerm_resource_group.primary.name
-  scope      = azurerm_resource_group.primary.id
-  lock_level = "CanNotDelete"
-  notes      = "Only IaC should modify"
-}
