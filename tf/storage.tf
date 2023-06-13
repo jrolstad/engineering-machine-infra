ef0460e060903cc119f9cd70083fc2469ea07e5b
Joshua Rolstad <joshuarolstad@precocityllc.com>
2023-06-12 19:57:18 -0700

    Shortening storageaccount name

--- tf/storage.tf
+++ tf/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "app" {
-  name                     = replace("${var.service_name}app${var.environment}", "-", "")
+  name                     = replace("${var.service_name}${var.environment}", "-", "")
   resource_group_name      = azurerm_resource_group.primary.name
   location                 = azurerm_resource_group.primary.location
   account_tier             = "Standard"
