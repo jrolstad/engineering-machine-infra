d33cfadac44e0d794aa65db04aa83ddaa31c6652
Joshua Rolstad <joshuarolstad@precocityllc.com>
2023-06-12 20:37:40 -0700

    Trying to get password to be accepted

--- tf/secrets.tf
+++ tf/secrets.tf
@@ -42,6 +42,7 @@ resource "random_password" "machine_admin" {
   min_upper        = 1
   min_numeric      = 1
   min_special      = 1
+  special          = true
   override_special = "!@#$%&*()-=+[]{}<>:?."
 }
 
